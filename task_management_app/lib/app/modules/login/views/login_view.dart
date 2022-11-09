2e11653f34358404a07c86939114291d5c116c1a
ShintaAull <shintaaulia4j4@gmail.com>
2022-11-10 00:41:32 +0700

    commit task

--- task_management_app/lib/app/modules/login/views/login_view.dart
+++ task_management_app/lib/app/modules/login/views/login_view.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -10,7 +13,72 @@ class LoginView extends GetView<LoginController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue[100],
-      body: Container(margin: EdgeInsets.all(get.height),)
+      body: Container(
+        margin: EdgeInsets.all(Get.height * 0.1),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.white,
+        ),
+        child: Row(children: [
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  bottomLeft: Radius.circular(50),
+                ),
+                color: Colors.blue,
+              ),
+              // ignore: prefer_const_literals_to_create_immutables
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      const Text(
+                        'Welcome',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 70,
+                        ),
+                      ),
+                      const Text(
+                        'Please Sign In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                        ),
+                      ),
+                      const Text(
+                        'Start Journey With US',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(50),
+                    bottomRight: Radius.circular(50),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    Image.asset('assets/images/image.png.jpeg'),
+                    FloatingActionButton.extended(
+                        onPressed: () {}, label: Text('Sign In With Google')),
+                  ],
+                )),
+          ),
+        ]),
       ),
     );
   }
