764db6a566bd2d3de5373d243b2cb70e5a21e051
ShintaAull <shintaaulia4j4@gmail.com>
2022-11-23 15:53:48 +0700

    commit tugas7

--- task_management_app/lib/app/utils/style/AppColors.dart
+++ task_management_app/lib/app/utils/style/AppColors.dart
@@ -1,1 +1,9 @@
-// TODO Implement this library.
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryBg = Color(0xFFBFE2FF);
+  static const primaryText = Color(0xFF808080);
+  static const cardBg = Color(0xffe3e3ee);
+}
